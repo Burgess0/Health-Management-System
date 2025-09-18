eac574e6d21155e7accc19c6235a951ab7aa7b67
Burgess0 <zhlong032@gmail.com>
2025-09-18 23:47:59 +0800

    first

--- 家乡馆-终版/Order/Models/IShopRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Order.Models
-{
-    public interface IShopRepository
-    {
-        IEnumerable<Shop> GetAllShops();//获取所有家乡馆信息
-        Shop GetShopById(int id);//根据家乡馆Id获取店铺信息
-    }
-}
